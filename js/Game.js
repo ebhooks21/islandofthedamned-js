5e25b17c6c72be1e5e2128a83378043cb104c994
Eric Hooks <ebhooks21@gmail.com>
2025-02-13 05:52:11 -0600

    Built the MainMenuController. Built the Game class.

---
+++ js/Game.js
@@ -0,0 +1,22 @@
+/**
+ * Game.js -- File for the main game class.
+ * @author Eric Hooks, 2025
+ */
+
+class Game {
+	/**
+	 * Main constructor for the Game class.
+	 */
+	constructor() {
+		let self = this;
+
+		self.mainMenuController = new MainMenuController();
+    }
+
+	/**
+	 * Function to start the Game.
+	 */
+	start() {
+
+	}
+}
---
+++ js/controllers/MainMenuController.js
@@ -0,0 +1,26 @@
+/**
+ * MainMenuController.js -- File to hold the main menu controller.
+ * @author Eric Hooks, 2025
+ */
+
+class MainMenuController {
+	/**
+	 * Constructor for the MainMenu.
+	 */
+	constructor() {
+    }
+
+	/**
+	 * Function to register the onclick functions for this controller.
+	 */
+	registerOnClickFunction() {
+
+	}
+
+	/**
+	 * Function for the New Game Button onclick.
+	 */
+	newGameButtonOnClick() {
+		alert("HERE");
+	}
+}
