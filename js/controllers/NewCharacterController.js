7154db6f146e222b25618e8a543ef02369e9af58
Eric Hooks <ebhooks21@gmail.com>
2025-02-13 22:49:05 -0600

    Added a function to fill in the character race listing area.

--- js/controllers/NewCharacterController.js
+++ js/controllers/NewCharacterController.js
@@ -53,6 +53,39 @@ class NewCharacterController {
 
 		//Show the main menu area
         $('#newcharacterarea').show();
+
+		//Build the area with the character races
+        self.buildCharacterRaceSelectionArea();
+    }
+
+    /**
+     * Function to build the character race selection area.
+     */
+	buildCharacterRaceSelectionArea() {
+		let self = this;
+
+		let html = "";
+		let state = "";
+		let races = ((self.game).gameRaceController).races;
+
+		for(let r in races) {
+			html += "<div class='form-check'>";
+
+			if(races[r].name == "Human") {
+				state = "checked";
+			}
+
+			else {
+				state = "";
+			}
+
+			html += "<input class='form-check-input' type='radio' name='newcharacterform-raceradio' id='newcharacterform-" + races[r].name + "=raceradiooption' " + state + ">";
+			html += "<label class='form-check-label' for='newcharacterform-'" + races[r].name + "=raceradiooption'>" + races[r].name + "</label>";
+
+			html += "</div>";
+		}
+
+		$("#newcharacterform-racelistingarea").html(html);
 	}
 
 	/**
