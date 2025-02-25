aca6b3cb49cf8c30df906d0ca0fcb4217af8f279
Eric Hooks <ebhooks21@gmail.com>
2025-02-24 23:27:11 -0600

    Added Character classes to the new character screen.

--- js/controllers/NewCharacterController.js
+++ js/controllers/NewCharacterController.js
@@ -56,6 +56,9 @@ class NewCharacterController {
 
 		//Build the area with the character races
         self.buildCharacterRaceSelectionArea();
+
+		//Build the area for the character classes
+		self.buildCharacterClassSelectionArea();
     }
 
 	/**
@@ -67,18 +70,19 @@ class NewCharacterController {
 		//Get the character's currently selected race
 		let race = self.getSelectedCharacterRace();
 
-		console.log(race);
+		//Get the character's currently selected class
+		let characterClass = self.getSelectedCharacterClass();
 
         //Update the character stats area with the selected race's stats block
 		let baseStatBlock = new StatBlock(5, 5, 5, 5, 5, 5,);	
 
 		//Update the base stat block with the race stat information
-		baseStatBlock.strength += (race.statblock).strength;
-		baseStatBlock.agility += (race.statblock).agility;
-		baseStatBlock.intelligence += (race.statblock).intelligence;
-		baseStatBlock.charisma += (race.statblock).charisma;
-		baseStatBlock.constitution += (race.statblock).constitution;
-		baseStatBlock.luck += (race.statblock).luck;
+		baseStatBlock.strength += ((race.statblock).strength + (characterClass.statblock).strength);
+		baseStatBlock.agility += ((race.statblock).agility + (characterClass.statblock).agility);
+		baseStatBlock.intelligence += ((race.statblock).intelligence + (characterClass.statblock).intelligence);
+		baseStatBlock.charisma += ((race.statblock).charisma + (characterClass.statblock).charisma);
+		baseStatBlock.constitution += ((race.statblock).constitution + (characterClass.statblock).constitution);
+		baseStatBlock.luck += ((race.statblock).luck + (characterClass.statblock).luck);
 
 		//Update the stat display
 		$('#newcharacterarea-statsarea-strength').val(baseStatBlock.strength);
@@ -100,6 +104,21 @@ class NewCharacterController {
 		for(let r in races) {
 			if($('#newcharacterform-' + races[r].name + '-raceradiooption').is(':checked')) {
 				return races[r];
+			}
+		}
+	}
+
+	/**
+	 * Function to get the selected character class.
+	 */
+	getSelectedCharacterClass() {
+		let self = this;
+		
+		let classes = ((self.game).gameEntityClassController).classes;
+
+		for(let c in classes) {
+			if($('#newcharacterform-' + classes[c].name + '-classradiooption').is(':checked')) {
+				return classes[c];
 			}
 		}
 	}
@@ -143,7 +162,49 @@ class NewCharacterController {
 				self.buildCharacterStatsArea();
             });
 		}
-		$('#newcharacterform-Human-raceradiooption').trigger('click');
+		//$('#newcharacterform-Human-raceradiooption').trigger('click');
+	}
+
+	/**
+     * Function to build the character class selection area.
+     */
+	buildCharacterClassSelectionArea() {
+		let self = this;
+
+		let html = "";
+		let state = "";
+		let classes = ((self.game).gameEntityClassController).classes;
+
+		for(let c in classes) {
+			html += "<div class='form-check float-lg-start me-lg-4 col-12'>";
+
+			if(classes[c].name == "Warrior") {
+				state = "checked";
+			}
+
+			else {
+				state = "";
+			}
+
+			html += "<input class='form-check-input' type='radio' name='newcharacterform-classradio' id='newcharacterform-" + classes[c].name + "-classradiooption' " + state + ">";
+			html += "<label class='form-check-label' for='newcharacterform-" + classes[c].name + "-classradiooption'>" + classes[c].name + "</label>";	
+
+			html += "</div>";
+		}
+
+		$('#newcharacterform-classlistingarea').html(html);
+
+		for(let c in classes) {
+			//Set an onclick for each radio button, so that it updates the description area
+			$("#newcharacterform-" + classes[c].name + "-classradiooption").on('click', () => {
+				$('#newcharacterform-descriptiontitle').html(classes[c].name);
+				$('#newcharacterform-descriptiontext').html(classes[c].description);
+
+				//Change the character stats area
+				self.buildCharacterStatsArea();
+            });
+		}
+		$('#newcharacterform-Warrior-classradiooption').trigger('click');
 	}
 
 	/**
